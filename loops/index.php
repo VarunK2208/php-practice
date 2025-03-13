48b0baeba681ac1d7347b774e6955a89592a2170
Varun Kallepally <varun.k@snapperit.com>
2025-03-13 06:10:20 +0000

    loops

--- loops/index.php
+++ loops/index.php
@@ -36,6 +36,32 @@ switch ($z) {
     break;
     }
 
+    for($v = 0;$v<10;$v++) {
+        echo $v;
+    }
+
+    $q = 1;
+    while($q <=10) {
+        echo $q;
+        $q++;
+    }
+
+    $w = 1;
+    do {
+        echo $w;
+        $w++;
+        } while ($w <= 10);
 
 
+$ar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20];
+foreach ($ar as $value) {
+    if($value % 2 == 0) {
+        continue;
+        }
+        echo $value;
+        
+        if ($value === 13) {
+            break;
+            }
+}
 ?>
